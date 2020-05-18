644fa96bc1327cab76d697612f8e2fe28e0cd607
kyon12345 <a13576034204@163.com>
2020-05-18 15:32:26 +0800

    添加日志

--- EFCoreCourseTest/EFCoreDataContext.cs
+++ EFCoreCourseTest/EFCoreDataContext.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Models;
 
 namespace EFCoreCourseTest {
     public class EFCoreDataContext : DbContext {
+        public static readonly ILoggerFactory loggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
         protected override void OnConfiguring (DbContextOptionsBuilder builder) {
-            builder.UseSqlServer ("Server=(localdb)\\mssqllocaldb;Database=EFCoreDb;Trusted_Connection=True;");
+            builder.UseLoggerFactory(loggerFactory).
+            UseSqlServer ("Server=(localdb)\\mssqllocaldb;Database=EFCoreDb;Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating (ModelBuilder builder) {

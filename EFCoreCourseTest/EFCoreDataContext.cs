73ed4d4cbea69002894fec63d45dd65c2e3ab7cb
kyon12345 <a13576034204@163.com>
2020-05-18 09:21:02 +0800

    值转换器

--- EFCoreCourseTest/EFCoreDataContext.cs
+++ EFCoreCourseTest/EFCoreDataContext.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Models;
 
 namespace EFCoreCourseTest {
@@ -39,7 +40,28 @@ namespace EFCoreCourseTest {
                 // b.Property(p => p.Color).HasConversion(typeof(string)).HasMaxLength(10);
                 //映射字符串
                 b.Property(p => p.Color).HasColumnType("VARCHAR(20)");
+
+                //值转换器,加密和解密密码字段
+                b.Property(p => p.Password).HasConversion(v => Encyrpt(v),v=>Decyrpt(v));
+
+                //内置的值转换器,默认的转化false为0,true为1
+                // b.Property(i => i.IsMine).HasMaxLength(1).IsUnicode(false).HasConversion(typeof(string));
+
+                //自定义的值转换器
+                var boolConverter = new ValueConverter<bool, string>(v => v ? "X" : "", v => v == "X");
+                b.Property(i => i.IsMine).HasMaxLength(1).IsUnicode(false).HasConversion(boolConverter);
+
             });
+        }
+
+        string Encyrpt(string Password)
+        {
+            return String.Empty;
+        }
+
+        string Decyrpt(string Password)
+        {
+            return String.Empty;
         }
 
         public override int SaveChanges()
--- Models/Blog.cs
+++ Models/Blog.cs
@@ -13,6 +13,8 @@ namespace Models
         public DateTime CreateDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public Color Color { get; set; }
+        public string Password { get; set; }
+        public bool IsMine { get; set; }
     }
 
     // public enum Color:byte 老版本需要继承自byte

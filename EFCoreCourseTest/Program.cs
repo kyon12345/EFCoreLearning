bb7055f3e02e29f36664273f047b7349bdb1be5d
kyon12345 <a13576034204@163.com>
2020-05-20 09:32:17 +0800

    查询的三种方式

--- EFCoreCourseTest/Program.cs
+++ EFCoreCourseTest/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace EFCoreCourseTest {
     class Program {
         static void Main (string[] args) {
             var context = new EFCoreDataContext ();
-            context.Database.EnsureDeleted();
+            // context.Database.EnsureDeleted();
             context.Database.EnsureCreated ();
-            Console.ReadKey ();
+
+            //带有Tracking的查询
+            var stopTracker = new Stopwatch();
+            stopTracker.Start();
+
+            // // var blogs = context.Blogs.ToList();
+            // var blogs = context.Blogs.AsNoTracking().ToList();
+            // stopTracker.Stop();
+
+            //饥饿加载
+            // var blog= context.Blogs.Include(b => b.Posts).FirstOrDefault();
+            // var blog = context.Blogs.AsNoTracking().Include(b => b.Posts).FirstOrDefault();
+
+            //显式的加载,分为两次查询,性能较低
+            var blog = context.Blogs.Find(1);
+            context.Entry(blog)
+            .Collection(b => b.Posts)
+            .Load();
+
+            //6184
+            //1415
+            Console.WriteLine($"花费时间为{stopTracker.ElapsedMilliseconds}");
+
+            
+
+
+            Console.ReadKey (); 
         }
     }
 }
--- Models/Post.cs
+++ Models/Post.cs
@@ -3,6 +3,9 @@ namespace Models
     public class Post
     {
         public int Id { get; set; }
-        public string PostName { get; set; }
+        public int? BlogId { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public Blog Blog { get; set; }
     }
 }
